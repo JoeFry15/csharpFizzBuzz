15b665fa4007d0aaae6072b7d6b1a9b0c9bad5b2
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-02-20 12:03:19 +0000

    Add basic rules and max number selection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please input a maximum number: ");
+string maxNum = Console.ReadLine()!;
+
+for (int i = 1; i <= int.Parse(maxNum); i++)
+{
+    List<string> output = new List<string>();
+
+    if (i % 3 == 0)
+    {
+        output.Add("Fizz");
+    }
+
+    if (i % 13 == 0)
+    {
+        output.Add("Fezz");
+    }
+
+    if (i % 5 == 0)
+    {
+        output.Add("Buzz");
+    }
+
+    if (i % 7 == 0)
+    {
+        output.Add("Bang");
+    }
+
+    if (i % 11 == 0)
+    {
+        output.Clear();
+        if (i % 13 == 0)
+        {
+            output.Add("Fezz");
+        }
+        output.Add("Bong");
+    }
+
+    if (i % 17 == 0)
+    {
+        output.Reverse();
+    }
+
+    if (output.Any())
+    {
+        Console.WriteLine(string.Join("", output));
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
